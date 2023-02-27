34ace3dda4269a68ca4f6084d20e0584ef1c6e8a
SolmazMikayilova <mikayilovasolmaz2@gmail.com>
2023-02-28 02:35:17 +0400

    initial commit

---
+++ loop.js
@@ -0,0 +1,45 @@
+
+//task5
+/*let M=100;
+var count=0;
+
+for(let i=1;i<M;i++){
+    if(i%15==0){
+    count++;}
+}
+console.log(count);  */
+
+//task3
+/*let n=100;
+for(let i=1;i<=n;i++){
+    if(i%2==0){
+        console.log("true " +i);
+    }else{
+        console.log("false " +i);
+    }
+}  */
+
+
+//task4
+/*let n=21 ;
+let check=0;
+for(let i=1;i<=n;i++){
+  if(n%i==0){
+    check++;
+  }
+}
+if(check==2){
+    console.log("prime");
+}else{
+    console.log("composite");
+} */
+
+
+
+
+
+
+
+
+
+
